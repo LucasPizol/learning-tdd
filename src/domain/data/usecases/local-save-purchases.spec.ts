d9aa97ae06ebd31fa6ef62172f821e1afc3407c1
Lucas Pizol Ferreira <lucaspizolfe@gmail.com>
2024-07-28 18:52:33 -0300

    make sut helper method

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,19 +1,53 @@
 class LocalSavePurchases {
   constructor(private readonly cacheStore: CacheStore) {}
+
+  async save(): Promise<void> {
+    await this.cacheStore.delete();
+  }
 }
 
 interface CacheStore {
-  deleteCallsCount: number;
+  delete(): Promise<void>;
 }
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0;
+
+  async delete(): Promise<void> {
+    this.deleteCallsCount++;
+  }
 }
+
+type SutTypes = {
+  sut: LocalSavePurchases;
+  cacheStore: CacheStoreSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalSavePurchases(cacheStore);
+  return {
+    sut,
+    cacheStore,
+  };
+};
 
 describe("LocalSavePurchases", () => {
   test("Should not delete cache on sut.init", () => {
-    const cacheStore = new CacheStoreSpy();
+    const { cacheStore } = makeSut();
     new LocalSavePurchases(cacheStore);
     expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+
+  test("Should delete old cache on sut.save", async () => {
+    const { cacheStore, sut } = makeSut();
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
+  });
+
+  test("Should delete old cache on sut.save", async () => {
+    const { cacheStore, sut } = makeSut();
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
   });
 });
